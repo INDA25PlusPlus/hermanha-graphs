e1a846a095ade5146943b49a1418d035e798f29b
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2026-02-04 21:55:23 +0100

    updated single_shortes_path, with dijkstra, much quicker solution

--- single_shortest_path/single_shortest_path.rs
+++ single_shortest_path/single_shortest_path.rs
@@ -1,8 +1,62 @@
-// Submission ID: 19083435
+// Submission ID: 19107021
 
-use std::{io::{self, Read}, vec};
+use std::{cmp::Reverse, collections::BinaryHeap, io::{self, Read}, vec};
 
 const INF: u64 = 1_000_000_000;
+
+struct Graph {
+    edges: Vec<Vec<(usize, u64)>>
+}
+
+impl Graph {
+    pub fn new(n: usize) -> Self {
+        Self {
+            edges: vec![Vec::new(); n],
+        }
+    }
+
+    fn make_adjacent(&mut self, edges: &[(usize, usize, u64)]) {
+        for &(u,v,w) in edges {
+            self.edges[u].push((v,w))
+        }
+    }
+
+    fn distance(&self, u: usize, v: usize) -> u64{
+        for &(to, w) in &self.edges[u] {
+            if to == v{
+                return w
+            }
+        }
+        INF
+    }
+}
+
+fn dijkstra(
+    edges: &[(usize, usize, u64)],
+    start: usize,
+    n: usize,
+) -> Vec<u64> {
+    let mut graph = Graph::new(n);
+    graph.make_adjacent(edges);
+
+    let mut dist = vec![INF;n];
+    dist[start] = 0;
+
+    let mut heap = BinaryHeap::new();
+    heap.push((Reverse(0u64), start));
+    
+    while let Some((Reverse(d), u)) = heap.pop() {
+        for &(v,w) in &graph.edges[u] {
+            let alt = dist[u] + w;
+            if alt < dist[v] {
+                dist[v] = alt;
+                heap.push((Reverse(alt), v));
+            }
+        }
+    }
+
+    dist
+}
 
 fn bellman_ford(
     edges: &[(usize, usize, u64)],
@@ -62,7 +116,7 @@ fn main() {
             edges.push((u,v,w));
         }
 
-        let distances = bellman_ford(&edges, s, n);
+        let distances = dijkstra(&edges, s, n);
 
         for _ in 0..q {
             let end = it.next().unwrap() as usize;
